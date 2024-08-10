2ddb4ea5d84150b8f72e869bac572aed70e9a627
Bas <57452503+basekkelenkamp@users.noreply.github.com>
2024-08-10 16:53:00 +0200

    update requirements pre-commit

--- export-requirements.ps1
+++ export-requirements.ps1
@@ -1,3 +1,8 @@
+# Export dependencies to requirements.txt without hashes
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 
+# Read the requirements.txt and remove lines with 'python_full_version' conditions
+(Get-Content requirements.txt) -replace '\s;\s*.*?python_full_version\s*==\s*".*?"(\s*and\s*.*?|\s*or\s*.*?)*', '' | Set-Content requirements.txt
+
+# Stage the updated requirements.txt for commit
 git add requirements.txt
